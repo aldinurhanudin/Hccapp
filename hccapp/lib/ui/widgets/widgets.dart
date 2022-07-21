1bd25007d16e9175424aee0c4b5965b92705aa20
Aldi Nurhanudin <aldinurhanudin08@gmail.com>
2022-07-21 15:28:20 +0700

    revisi border dan navigation ke pdf

--- hccapp/lib/ui/widgets/widgets.dart
+++ hccapp/lib/ui/widgets/widgets.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get_core/get_core.dart';
+import 'package:get/get_navigation/src/extension_navigation.dart';
 import 'package:hccapp/shared/shared.dart';
+import 'package:hccapp/ui/pages/edit_profile_page.dart';
+import 'package:hccapp/ui/pages/pages.dart';
 
 part 'absen_card.dart';
 part 'fitur_item.dart';
